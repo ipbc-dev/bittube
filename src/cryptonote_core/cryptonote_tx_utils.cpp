a954c27671ad4c8f7cc844fa6e896b30b670c632
Markus Behm <markus@ipbc.io>
2018-06-12 17:13:41 +0000

    Disabled block_reward dust low significant digits cutoff

--- src/cryptonote_core/cryptonote_tx_utils.cpp
+++ src/cryptonote_core/cryptonote_tx_utils.cpp
@@ -257,9 +257,11 @@ namespace cryptonote
     // from hard fork 4, we use a single "dusty" output. This makes the tx even smaller,
     // and avoids the quantization. These outputs will be added as rct outputs with identity
     // masks, to they can be used as rct inputs.
+    /*
     if (hard_fork_version >= BLOCK_MAJOR_VERSION_2 && hard_fork_version < BLOCK_MAJOR_VERSION_4) {
       block_reward = block_reward - block_reward % ::config::BASE_REWARD_CLAMP_THRESHOLD;
     }
+    */
 
     std::vector<uint64_t> out_amounts;
     decompose_amount_into_digits(block_reward, hard_fork_version >= BLOCK_MAJOR_VERSION_2 ? 0 : ::config::DEFAULT_DUST_THRESHOLD,
