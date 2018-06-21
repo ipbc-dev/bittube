c53f0bc2c1f4ef0dd06a04f3034095b20d22ea84
Markus Behm <markus@ipbc.io>
2018-06-21 13:54:21 +0200

    Fixed max transaction version for hard fork v4

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -2694,7 +2694,7 @@ bool Blockchain::check_tx_inputs(transaction& tx, tx_verification_context &tvc, 
     }
 
     // min/max tx version based on HF, and we accept v1 txes if having a non mixable
-    const size_t max_tx_version = (hf_version <= BLOCK_MAJOR_VERSION_4) ? 1 : 2;
+    const size_t max_tx_version = (hf_version <= BLOCK_MAJOR_VERSION_3) ? 1 : 2;
     if (tx.version > max_tx_version)
     {
       MERROR_VER("transaction version " << (unsigned)tx.version << " is higher than max accepted version " << max_tx_version);
