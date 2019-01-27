4ac943cab2bde8120a39f797ddff099f253eda89
italocoin <info@italocoin.com>
2019-01-27 16:02:56 -0500

    branding here and there

--- src/wallet/api/wallet2_api.h
+++ src/wallet/api/wallet2_api.h
@@ -1185,7 +1185,7 @@ struct WalletManager
     //! stops mining
     virtual bool stopMining() = 0;
 
-    //! resolves an OpenAlias address to a monero address
+    //! resolves an OpenAlias address to a bittube address
     virtual std::string resolveOpenAlias(const std::string &address, bool &dnssec_valid) const = 0;
 
     //! checks for an update and returns version, hash and url
